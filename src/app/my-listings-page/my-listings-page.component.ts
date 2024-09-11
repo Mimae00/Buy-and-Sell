38eeefb45e5689f9685a83a7ab6dd1189d3df563
Michael <garbosamichael1@gmail.com>
2024-09-11 21:24:34 +0800

    Added my listing page functions

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Listing } from '../types';
+import { fakeMyListings } from '../fake-data';
+import { RouterModule } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-my-listings-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,NgFor],
   templateUrl: './my-listings-page.component.html',
   styleUrl: './my-listings-page.component.css'
 })
 export class MyListingsPageComponent {
+  
+  listings: Listing[] = [];
 
+  ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string): void { //not working
+    alert(`Deleting your listing with id ${listingId}`);
+  }
 }
