d5269be8d907162249b28eb582dfe0ac155fff1b
Michael <garbosamichael1@gmail.com>
2024-09-11 22:05:26 +0800

    added new listing page function

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FormsModule],
   templateUrl: './new-listing-page.component.html',
   styleUrl: './new-listing-page.component.css'
 })
 export class NewListingPageComponent {
 
+  name: string ='';
+  description: string ='';
+  price: string ='';
+
+  constructor(
+    private router: Router,
+  ){}
+
+  onSubmit(): void {
+    alert('Creating a new listing . . .');
+    this.router.navigateByUrl('/my-listings');
+  }
 }
